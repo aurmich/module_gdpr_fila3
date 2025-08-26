c3a0615e88e8690eaca7db5457931dc442275e6c
Malebestia <malebestia@yahoo.it>
2025-08-26 18:04:41 +0200

    .

--- tests/Pest.php
+++ tests/Pest.php
@@ -16,6 +16,16 @@ use Modules\Gdpr\Tests\TestCase;
 */
 
 pest()->extend(TestCase::class)
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+=======
+>>>>>>> f0c1e02 (.)
+=======
+>>>>>>> d3f4a5d (.)
+>>>>>>> da11c8a (.)
     ->in('Feature', 'Unit');
 
 /*
